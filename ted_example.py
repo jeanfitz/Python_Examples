d1853682611d6b1847896adf863da1f9052dbe5b
Jean Fitzmaurice <54041487+jeanfitz@users.noreply.github.com>
2020-05-16 21:19:03 -0400

    Add files via upload

---
+++ ted_example.py
@@ -0,0 +1,17 @@
+# simple python script (.py file)  
+# run in same directory you start python3 in ---
+# 
+# python3 ted_example.py
+
+# JAF May 2020 
+
+a="Ted"
+b="Terpstra"
+
+print(a + ' ' +b)
+
+c="Data Science Guru"
+
+print(a + ' ' + b + ' is a  '+ c)
+
+
